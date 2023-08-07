b3d089aa24022ffaadbdbe630d151fbe6ffbdd5c
Epuri <yoga.narasimha-epuri@capgemini.com>
2023-08-07 14:46:43 +0530

    added Folder Strcture template.py code

---
+++ template.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "mlproject"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+    "templates/index.html"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath) 
+    filedir, filename = os.path.split(filepath)  # filedir relative to the current parent directory.
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+    
+    if (not os.path.exists(filepath)):
+        with open(filepath, "w"):
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is already exists")
+
+
+"""
+    1. Project Structure.
+    2. Github won't save the empty folder, so .gitkeep file created. (.github will use for deployements using github actions).
+    3. -e ., look for setup.py (Inform about the information of local package)
+"""
